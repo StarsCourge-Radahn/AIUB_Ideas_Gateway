43b8f4b9bc2d4d55955d24ccc76a4e9925a9d86e
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-08-05 20:56:02 +0600

    Register user tested🏷️

--- BLL/DTOs/UserDTO.cs
+++ BLL/DTOs/UserDTO.cs
@@ -12,6 +12,6 @@ namespace BLL.DTOs
         public string Name { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public string Role { get; set; }
+        public string Role { get; set; } = "user";
     }
 }
