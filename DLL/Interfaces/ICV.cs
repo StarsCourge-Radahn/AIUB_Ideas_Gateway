c6626aeb35887f8c8b96dc223801b5994b1621c2
sadhiin <sanjidulislamsadhin1994@gmail.com>
2023-08-17 23:38:21 +0600

    Begining the cv oparetions

---
+++ DLL/Interfaces/ICV.cs
@@ -0,0 +1,16 @@
+﻿using DLL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Interfaces
+{
+    public interface ICV
+    {
+        bool CreateCV(CV obj);
+        List<CV>GetAllCV();
+
+    }
+}
---
+++ DLL/Repos/CVRepo.cs
@@ -0,0 +1,23 @@
+﻿using DLL.EF.Models;
+using DLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL.Repos
+{
+    internal class CVRepo : DataRepository, ICV
+    {
+        public bool CreateCV(CV obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CV> GetAllCV()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
