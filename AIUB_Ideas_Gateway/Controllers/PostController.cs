a38e4c4bf2c5508cac623135bc97bbea2fe49d03
Saurav Sarkar <sauravsarkar260@gmail.com>
2023-08-27 12:17:12 +0600

    add frontend Use html css angular also chng on Postservice

--- AIUB_Ideas_Gateway/Controllers/PostController.cs
+++ AIUB_Ideas_Gateway/Controllers/PostController.cs
@@ -8,9 +8,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace AIUB_Ideas_Gateway.Controllers
 {
+    [EnableCors("*", "*", "*")]
     [LoggedIn]
     public class PostController : ApiController
     {
